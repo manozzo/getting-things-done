a3744ba347c534446a9d23cec16c044d6763923c
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-08-18 16:26:33 -0300

    remove calendar.js component

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,3 @@ export { default as Title } from "./title";
 export { default as LoginForm } from "./loginform";
 export { default as TaskModal } from "./modal";
 export { default as Tarefa } from "./tarefa";
-export { default as Calendar } from "./calendar";
